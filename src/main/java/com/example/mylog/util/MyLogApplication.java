5a8376cfeb3ae7bf9732dea1767d9b03fe91b9ca
zxm <1069406508@qq.com>
2021-07-05 19:15:45 +0800

    初始文件结构，day1首次提交

---
+++ src/main/java/com/example/mylog/util/MyLogApplication.java
@@ -0,0 +1,13 @@
+package com.example.mylog.util;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyLogApplication.class, args);
+    }
+
+}
